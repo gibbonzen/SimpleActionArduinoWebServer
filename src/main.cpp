fe4ea48b7b18f8783ce150d537470a2f5be2aac2
Gibbon <julian.mauborgne@gmail.com>
2024-01-07 13:15:06 +0100

    add some comments

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ IPAddress subnet(255, 255, 255, 0);
 
 WebServer server(80);
 
+// Declare methods
 void setState();
 void handleRequest();
 void handleOn();
@@ -20,8 +21,9 @@ void sendHTML();
 void sendJSON();
 String buildHTML();
 
-const uint8_t PIN = 37;
-bool state = LOW;
+// Define model properties
+const uint8_t PIN = 37; // ESP pin action
+bool state = LOW; // initial pin state
 
 void off()
 {
@@ -37,7 +39,7 @@ void on()
 
 void setState()
 {
-  digitalWrite(PIN, state);
+  digitalWrite(PIN, state); // update pin state
 }
 
 void setup()
@@ -46,10 +48,12 @@ void setup()
 
   off();
 
+  // Run wifi on access point mode
   WiFi.softAP(SSID, PWD);
   WiFi.softAPConfig(localIp, gateway, subnet);
   delay(10);
 
+  // Initialize endpoints 
   server.on("/", handleRequest);
   server.on("/on", handleOn);
   server.on("/off", handleOff);
@@ -61,36 +65,45 @@ void setup()
 
 void loop()
 {
-  server.handleClient();
+  server.handleClient(); // Handle web requests
 }
 
 void handleRequest()
 {
-  sendHTML();
+  sendHTML(); // publish html page
 }
 
 void handleOn()
 {
-  on();
-  sendJSON();
+  on(); // eneable arduino pin to HIGH
+  sendJSON(); // send json response to current request
 }
 
 void handleOff()
 {
-  off();
+  off(); // disable pin 
   sendJSON();
 }
 
-void handleToggle() {
-  if(state == LOW) {
-    on();
+/**
+ * Eneable/ disable pin depends on current state
+*/
+void handleToggle() 
+{
+  if (state == LOW)
+  {
+    handleOn();
   }
-  else {
-    off();
+  else
+  {
+    handleOn();
   }
-  sendJSON();
 }
 
+/**
+ * Build json response with current pin state 
+ * and send it 
+*/
 void sendJSON()
 {
   String textState = state == LOW ? "false" : "true";
@@ -99,43 +112,58 @@ void sendJSON()
   server.send(200, "application/json", response);
 }
 
+/**
+ * Send the html page
+*/
 void sendHTML()
 {
   server.send(200, "text/html", buildHTML());
 }
 
-String getStateTextButton() {
+/**
+ * Write "Activée" or "Désactivée" depends on current pin state
+*/
+String getStateTextButton()
+{
   return state == LOW ? "Désactivée" : "Activée";
 }
 
+/**
+ * Build html page with a single button
+ * The button call the "toggle" endpoint define on the web server
+ * and get back the json response with the pin state to update the display of button name
+ * 
+ * The html page is build the first time the "/" endpoint is called and 
+ * datas are refreshed by the javascript script (included in the html page) on each click on the button. 
+*/
 String buildHTML()
 {
   String html = "<!DOCTYPE html>";
-html += "<html lang=\"en\">";
-html += "<head>";
-html += "    <meta charset=\"UTF-8\">";
-html += "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
-html += "    <title>Prises connectées</title>";
-html += "</head>";
-html += "<body>";
-html += "    <label for=\"device_1\">Prise n°1 (Guirlande extérieure) </label>";
-html += "    <button id=\"device_1\" onclick=\"activer(1)\">" + getStateTextButton() + "</button>";
-html += "    <script type=\"application/javascript\">";
-html += "        const device_1 = document.getElementById(\"device_1\");";
-html += "        const activer = async (name) => {";
-html += "            await fetch(\"http://192.168.0.1/toggle\")";
-html += "                .then(response => response.json())";
-html += "                .then(data => {";
-html += "                    if(data.state) {";
-html += "                        device_1.textContent = \"Activée\";";
-html += "                    }";
-html += "                    else {";
-html += "                        device_1.textContent = \"Désactivée\";";
-html += "                    }";
-html += "                })";
-html += "        }";
-html += "    </script>";
-html += "</body>";
-html += "</html>";
+  html += "<html lang=\"en\">";
+  html += "<head>";
+  html += "    <meta charset=\"UTF-8\">";
+  html += "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
+  html += "    <title>Prises connectées</title>";
+  html += "</head>";
+  html += "<body>";
+  html += "    <label for=\"device_1\">Prise n°1 (Guirlande extérieure) </label>";
+  html += "    <button id=\"device_1\" onclick=\"activer(1)\">" + getStateTextButton() + "</button>";
+  html += "    <script type=\"application/javascript\">";
+  html += "        const device_1 = document.getElementById(\"device_1\");";
+  html += "        const activer = async (name) => {";
+  html += "            await fetch(\"http://192.168.0.1/toggle\")";
+  html += "                .then(response => response.json())";
+  html += "                .then(data => {";
+  html += "                    if(data.state) {";
+  html += "                        device_1.textContent = \"Activée\";";
+  html += "                    }";
+  html += "                    else {";
+  html += "                        device_1.textContent = \"Désactivée\";";
+  html += "                    }";
+  html += "                })";
+  html += "        }";
+  html += "    </script>";
+  html += "</body>";
+  html += "</html>";
   return html;
 }
